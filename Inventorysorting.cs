b607d503e497f338e4166307b1e5d4f0a0a7539f
Florian <flohirson@gmail.com>
2018-01-30 12:47:53 +0000

    stack all, ut still have a empty stack issues

--- Inventorysorting.cs
+++ Inventorysorting.cs
@@ -9,7 +9,10 @@ using Eleon.Modding;
 public partial class InventorySorting: ModInterface
 {
     static ModGameAPI GameAPI;
-    
+    ushort eventSortId = 26989;
+    ushort eventSetInventoryId = 26980;
+
+
     public void Game_Start(ModGameAPI dediAPI)
     {
         InventorySorting.GameAPI = dediAPI;
@@ -32,13 +35,16 @@ public partial class InventorySorting: ModInterface
     {
 
         GameAPI.Console_Write($"ID:EVENT! {eventId} - {seqNr}");
+        LogFile("chat.txt", $"ID:EVENT! {eventId} - {seqNr}");
         try
         {
             switch (eventId)
             {
 
                 case CmdId.Event_Player_Connected:
-                    GameAPI.Console_Write("InventorySorting : player connected! ");
+                    int id = (int)data;
+                    GameAPI.Console_Write("SuicideCommand : player " + id + "connected! ");
+                    LogFile("chat.txt", "Player " + id + "connected");
                     break;
                 case CmdId.Event_ChatMessage:
                     ChatInfo ci = (ChatInfo)data;
@@ -49,14 +55,15 @@ public partial class InventorySorting: ModInterface
                     ci.msg = ci.msg.ToLower();
                     if (ci.msg.StartsWith("/sort"))
                     {
-                        int id = ci.playerId;
-                        GameAPI.Game_Request(CmdId.Request_Player_GetInventory, 2015, new Eleon.Modding.Id(id));
+                        id = ci.playerId;
+                        LogFile("chat.txt", "Player " + id + " sorted inventory");
+                        GameAPI.Game_Request(CmdId.Request_Player_GetInventory, eventSortId, new Eleon.Modding.Id(id));
                         
                     }
                     break;
                 case CmdId.Event_Player_Inventory:
                     Inventory inventory = (Inventory) data;
-                    if(seqNr == 2015)
+                    if(seqNr == eventSortId)
                     {
                         Sort(inventory);
                     }
@@ -85,6 +92,7 @@ public partial class InventorySorting: ModInterface
     public void Sort(Inventory inventory)
     {
         GameAPI.Console_Write("InventorySorting : beginning sorting ");
+        LogFile("chat.txt", "InventorySorting : beginning sorting ");
         ItemStack[] bag = inventory.bag;
 
         for (int i = bag.Length - 1; i > 0; i--)
@@ -94,23 +102,17 @@ public partial class InventorySorting: ModInterface
                 if (bag[j].id == bag[j + 1].id)
                 {
                     int sum = bag[j].count + bag[j + 1].count;
-                    if (sum <= 1000  && bag[j].ammo == 0) //check if the sum doesn't exceed 1000 items and if its not a weapon
+                    if (bag[j].ammo == 0) //check if its not a weapon
                     {
                         bag[j].count = sum;
                         bag[j + 1] = new ItemStack();
-                        --j;
-                    } else if (sum > 1000 && sum < 2000 && bag[j].ammo == 0) //chck if the sum is between 1000 and 2000 and if its not a weapon
-                    {
-                        bag[j].count = 1000;
-                        int remain = sum - 1000;
-                        bag[j + 1].count = remain;
-                    }
+                        j = 0;
+                    } 
                 }
 
                 if (bag[j].id > bag[j + 1].id)
                 {
                     ItemStack highValue = bag[j];
-
                     bag[j] = bag[j + 1];
                     bag[j + 1] = highValue;
                 }
@@ -121,7 +123,8 @@ public partial class InventorySorting: ModInterface
 
         inventory.bag = bag;
         GameAPI.Console_Write("InventorySorting : sorting ended successfully ! ");
-        GameAPI.Game_Request(CmdId.Request_Player_SetInventory, (ushort) 2016, inventory);      
+        LogFile("chat.txt", "InventorySorting : sorting ended successfully ! ");
+        GameAPI.Game_Request(CmdId.Request_Player_SetInventory, eventSetInventoryId, inventory);      
             
         
     }
